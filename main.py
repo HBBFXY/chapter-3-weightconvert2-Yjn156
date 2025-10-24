7092bae140e3741833e77b0e6fdbb66fbc29d6d2
Yjn156 <yang14932860@qq.com>
2025-10-24 10:34:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-current_weight = 60
-moon_ratio = 0.165
-yearly_gain = 0.5
-years = 10
-print("未来10年地球和月球上的体重变化")
-for year in range(1,years + 1):
-    earth_weight = current_weight + yearly_gain * year
-    moon_weight = earth_weight * moon_ratio
-    print(f"第{year}年：地球上的体重{earth_weight:.2f}kg,月球上的体重{moon_weight:.2f}kg")
+# 在这个文件下编写代码，题目具体要求见README.md文件
+weight=input()
+if weight[-2:] in ["kg"]:
+    poung = (eval(weight[0:-2]))*2.2046
+    print("对应的英制重量为{:.3f}磅".format(poung))
+elif weight[-2:]in["pd"]:
+    kg = (eval(weight[0:-2]))*0.4535
+    print("对应的公制重量为{:.3f}公斤".format(kg))
+else:
+    print("输入错误")
